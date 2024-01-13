77251e616804cf7a9f028767574302f09f04df87
Nasren-khazaei <gonjishk90@gmail.com>
2024-01-13 14:20:50 +0330

    add js and varables

---
+++ script.js
@@ -0,0 +1,4 @@
+let list = document.querySelector("#to-do-list");
+let addBtn=document.querySelector("#add-form button");
+let addInput = document.querySelector("#add-form input");
+let searchInput = document.querySelector("#search-form input");
