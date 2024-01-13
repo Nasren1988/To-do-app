2fe288a37ac375dd6086a5e7affafc7f7213af9f
Nasren-khazaei <gonjishk90@gmail.com>
2024-01-13 14:37:05 +0330

    delet btn

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ let list = document.querySelector("#to-do-list");
 let addBtn=document.querySelector("#add-form button");
 let addInput = document.querySelector("#add-form input");
 let searchInput = document.querySelector("#search-form input");
+
+list.addEventListener("click",(e)=>{
+    if(e.target.nodeName =="SPAN" && e.target.className=="delete-btn"){
+       e.target.parentNode.remove();
+       if(list.children.length ==0){
+           let listEmptyMsg = document.createElement("div");
+           listEmptyMsg.style.color="#333";
+           listEmptyMsg.style.textAlign ="center";
+           listEmptyMsg.innerText="Your list is empty";
+           listEmptyMsg.id = "emptyMsg"
+           list.appendChild(listEmptyMsg);
+
+       }
+    }
+}) 
